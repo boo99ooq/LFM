302023dd2adebc0c5a7ff139f96d019deca132e9
boo99ooq <giusmasala99@gmail.com>
2026-01-21 14:57:30 +0100

    Update mercato.py

--- mercato.py
+++ mercato.py
@@ -260,4 +260,3 @@ elif menu == "4. Rose":
         st.dataframe(res_view[available_cols], use_container_width=True, hide_index=True)
     else:
         st.info("Nessun registro disponibile.")
-        
